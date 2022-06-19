58591d59fc311db2e0e383138914cd59e4b23f5f
Phi Thai <s3878070@rmit.edu.vn>
2022-06-19 08:06:49 +0700

    Initial Commit

---
+++ flag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flag
+//
+//  Created by Phi Thai on 19/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ flag/flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  flagApp.swift
+//  flag
+//
+//  Created by Phi Thai on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
